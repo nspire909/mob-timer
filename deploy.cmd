6f0b70f815af88e00b3a18252d536756e8096460
Nick Marks <nick.marks@eagletg.com>
2018-09-18 18:14:33 -0500

    ugh

--- deploy.cmd
+++ deploy.cmd
@@ -101,19 +101,19 @@ IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
 
 :: 3. Angular Prod Build //If you had generated this yourself then please add this step manually!!)
 IF EXIST "%DEPLOYMENT_SOURCE%/angular.json" (
-echo Building App in %DEPLOYMENT_SOURCE%…
-pushd "%DEPLOYMENT_SOURCE%"
-call :ExecuteCmd !NPM_CMD! run build
-:: If the above command fails comment above and uncomment below one
-:: call ./node_modules/.bin/ng build –prod
-IF !ERRORLEVEL! NEQ 0 goto error
-popd
+  echo Building App in %DEPLOYMENT_SOURCE%…
+  pushd "%DEPLOYMENT_SOURCE%"
+  call :ExecuteCmd !NPM_CMD! run build
+  :: If the above command fails comment above and uncomment below one
+  :: call ./node_modules/.bin/ng build –prod
+  IF !ERRORLEVEL! NEQ 0 goto error
+  popd
 )
 
 :: 4. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
-    call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%/dist/" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
-    IF !ERRORLEVEL! NEQ 0 goto error
+  call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%/dist/" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
+  IF !ERRORLEVEL! NEQ 0 goto error
 )
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
